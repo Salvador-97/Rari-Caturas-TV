0ccaca5336d20c073c08c50ecc6dad2dbe802bb5
Salvador-97 <salvador.go_97@hotmail.com>
2024-04-05 15:01:01 -0600

    estilo-barra-desplegable

--- src/controllers/controllerSeries.js
+++ src/controllers/controllerSeries.js
@@ -16,35 +16,6 @@ controller.start = (req, res) => {
     });
 };
 
-// controller.watch = (req, res) => {
-//     const { id } = req.params;
-//     // let idTemporada;
-//     //Conexion a la base de datos
-//     req.getConnection((error, conexion) => {
-//         conexion.query('SELECT * FROM seriesPrincipales WHERE idSerie = ?', [id], (error, serie) => {
-//             if (error) {
-//                 res.json(error);
-//             }
-//             var idTemporada = serie[0].temporada;
-//             res.render('watch', {
-//                 serie: serie[0]
-//             });
-//         });
-//         console.log(`idTemporda = ${idTemporada}`);
-//         // idTemporada = 'ATHF-1'
-//         conexion.query('SELECT * FROM capitulos WHERE idTemporada = ?', [idTemporada] , (error, capitulos) => {
-//             if (error) {
-//                 res.json(error);
-//             }
-//             console.log(`ID Temporada = ${idTemporada}`);
-//             console.log(typeof idTemporada);
-//             console.log(capitulos);
-//             // res.render('watch', {
-//             //     // serie: serie[0]
-//             // });
-//         });
-//     });
-// }
 
 controller.watch = (req, res) => {
     const { id } = req.params;
