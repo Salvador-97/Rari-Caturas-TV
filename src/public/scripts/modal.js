315f240fd1cee7fad64d0affda26d3fc723a5cf9
Salvador-97 <salvador.go_97@hotmail.com>
2025-07-28 22:11:02 -0600

    fix-modal

--- src/public/scripts/modal.js
+++ src/public/scripts/modal.js
@@ -1,19 +1,21 @@
 
-const open = document.getElementById('openModal');
-const modal = document.getElementById('mainModal');
-const close = document.getElementById('closeModal');
+document.addEventListener("DOMContentLoaded", () => {
+  const open = document.getElementById('openModal');
+  const modal = document.getElementById('mainModal');
+  const close = document.getElementById('closeModal');
+
+  open.addEventListener('click', () => {
+      modal.showModal();
+  });
+
+  close.addEventListener('click', () => {
+      modal.close();
+  });
+});
 
 const descargar = document.getElementById('modal-descargar');
 const modalDescargar = document.getElementById('mainModalDescarga');
 const closeDescarga = document.getElementById('closeModalD');
-
-open.addEventListener('click', () => {
-    modal.showModal();
-});
-
-close.addEventListener('click', () => {
-    modal.close();
-});
 
 descargar.addEventListener('click', () => {
     modalDescargar.showModal();
