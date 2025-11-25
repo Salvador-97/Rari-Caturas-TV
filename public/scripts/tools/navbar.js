5e6a90a357aca8579a8f67fe8d17c1dcb3360a09
Salvador-97 <salvador.go_97@hotmail.com>
2025-11-24 22:05:45 -0600

    fix-banner-serie

---
+++ public/scripts/tools/navbar.js
@@ -0,0 +1,32 @@
+const botonNav = document.querySelector('.btn-nav');
+
+botonNav.addEventListener('click', e => {
+    e.preventDefault();
+
+    const btnTemporadas = document.querySelector('.btn-temporadas');
+    const navLinks = document.querySelector('.nav-principal');
+    const links = document.querySelectorAll('.nav-links');
+
+    
+
+
+    //Tengo que habilitar todos los links
+    // Revisar cuando haga el cambio a fetch
+
+    //Meter toggle para quitar y poner clases
+
+    if (navLinks.classList.contains('habilitado')) {
+        navLinks.classList.remove('habilitado');
+        links.forEach(link => {
+            link.classList.add('link-deshabilitado');
+            link.classList.remove('link-habilitado')
+        });
+    } else {
+        navLinks.classList.add('habilitado');
+        links.forEach(link => {
+            link.classList.add('link-habilitado');
+            link.classList.remove('link-deshabilitado')
+        });
+    }
+
+})
